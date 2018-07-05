6567a2084b1c0c1d19e20a0df52c6f87f0314399
Jacky Tang <jackytck@gmail.com>
2018-07-06 00:30:07 +0800

    :hammer: refactor into link.go + use io.Reader

---
+++ ex4/link.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+	lane "gopkg.in/oleiade/lane.v1"
+)
+
+// Link represents a <link> tag
+type Link struct {
+	Href string
+	Text string
+}
+
+// Parse parses all the links in a reader.
+func Parse(r io.Reader) ([]Link, error) {
+	n, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseHTMLNode(n), nil
+}
+
+func parseHTMLNode(node *html.Node) []Link {
+	links := []Link{}
+
+	s := lane.NewStack()
+	s.Push(node)
+
+	for !s.Empty() {
+		t := s.Pop().(*html.Node)
+
+		if t.Type == html.ElementNode && t.Data == "a" {
+			links = append([]Link{parseLink(t)}, links...)
+		}
+
+		for c := t.FirstChild; c != nil; c = c.NextSibling {
+			s.Push(c)
+		}
+	}
+
+	return links
+}
+
+func parseLink(node *html.Node) Link {
+	return Link{
+		Href: parseLinkHref(node),
+		Text: parseLinkText(node),
+	}
+}
+
+func parseLinkHref(node *html.Node) string {
+	for _, a := range node.Attr {
+		if a.Key == "href" {
+			return a.Val
+		}
+	}
+	return ""
+}
+
+func parseLinkText(node *html.Node) string {
+	texts := []string{}
+
+	s := lane.NewStack()
+	s.Push(node)
+
+	for !s.Empty() {
+		t := s.Pop().(*html.Node)
+
+		if t.Type == html.TextNode {
+			text := strings.TrimSpace(t.Data)
+			texts = append([]string{text}, texts...)
+		}
+
+		if t.Type == html.ElementNode {
+			for c := t.FirstChild; c != nil; c = c.NextSibling {
+				s.Push(c)
+			}
+		}
+	}
+
+	return strings.TrimSpace(strings.Join(texts, " "))
+}
--- ex4/main.go
+++ ex4/main.go
@@ -4,101 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
-
-	"golang.org/x/net/html"
-	lane "gopkg.in/oleiade/lane.v1"
 )
 
 func main() {
 	filename := flag.String("html", "", "a html file")
 	flag.Parse()
 
-	links, err := ParseAllLinks(*filename)
+	file, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	links, err := Parse(file)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(links)
-}
-
-// ParseAllLinks parses all the links in a HTML file.
-func ParseAllLinks(filename string) ([]Link, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	n, err := html.Parse(file)
-	if err != nil {
-		return nil, err
-	}
-
-	return parseHTMLNode(n), nil
-}
-
-func parseHTMLNode(node *html.Node) []Link {
-	links := []Link{}
-
-	s := lane.NewStack()
-	s.Push(node)
-
-	for !s.Empty() {
-		t := s.Pop().(*html.Node)
-
-		if t.Type == html.ElementNode && t.Data == "a" {
-			links = append([]Link{parseLink(t)}, links...)
-		}
-
-		for c := t.FirstChild; c != nil; c = c.NextSibling {
-			s.Push(c)
-		}
-	}
-
-	return links
-}
-
-func parseLink(node *html.Node) Link {
-	return Link{
-		Href: parseLinkHref(node),
-		Text: parseLinkText(node),
-	}
-}
-
-func parseLinkHref(node *html.Node) string {
-	for _, a := range node.Attr {
-		if a.Key == "href" {
-			return a.Val
-		}
-	}
-	return ""
-}
-
-func parseLinkText(node *html.Node) string {
-	texts := []string{}
-
-	s := lane.NewStack()
-	s.Push(node)
-
-	for !s.Empty() {
-		t := s.Pop().(*html.Node)
-
-		if t.Type == html.TextNode {
-			text := strings.TrimSpace(t.Data)
-			texts = append([]string{text}, texts...)
-		}
-
-		if t.Type == html.ElementNode {
-			for c := t.FirstChild; c != nil; c = c.NextSibling {
-				s.Push(c)
-			}
-		}
-	}
-
-	return strings.TrimSpace(strings.Join(texts, " "))
-}
-
-// Link represents a <link> tag
-type Link struct {
-	Href string
-	Text string
 }
