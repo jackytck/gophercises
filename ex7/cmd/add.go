ca056c15fba14c142333552af73ce3999842a77d
Jacky Tang <jackytck@gmail.com>
2018-08-19 11:49:59 +0800

    :sparkles: cli shell

---
+++ ex7/cmd/add.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a task to your task list.",
+	Run: func(cmd *cobra.Command, args []string) {
+		task := strings.Join(args, " ")
+		fmt.Printf("Added \"%s\" to your task list.\n", task)
+	},
+}
---
+++ ex7/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jackytck/gophercises/ex7/cmd"
+
+func main() {
+	cmd.Execute()
+}
