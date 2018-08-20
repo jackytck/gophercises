33b07e93870c5ce9a0d6a5f3b80d5ad863e9c7c3
Jacky Tang <jackytck@gmail.com>
2018-08-20 23:53:39 +0800

    :sparkles: open boltdb with task bucket in home dir

---
+++ ex7/db/tasks.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+// Task represents a task.
+type Task struct {
+	key   int
+	Value string
+}
+
+// Init initializes a boltdb in dbPath.
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
+}
--- ex7/main.go
+++ ex7/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/jackytck/gophercises/ex7/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/jackytck/gophercises/ex7/cmd"
+	"github.com/jackytck/gophercises/ex7/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
 	cmd.Execute()
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
