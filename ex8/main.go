75b25d36b542f4096be858a8be720f119de9b0e7
Jacky Tang <jackytck@gmail.com>
2018-09-11 22:53:30 +0800

    :sparkles: find + update + delete phones

--- ex8/main.go
+++ ex8/main.go
@@ -51,7 +51,23 @@ func main() {
 	phones, err := allPhones(db)
 	must(err)
 	for _, p := range phones {
-		fmt.Printf("%+v\n", p)
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.number)
+		if number != p.number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := findPhone(db, number)
+			must(err)
+			if existing != nil {
+				// delete this number
+				must(deletePhone(db, p.id))
+			} else {
+				// update this number
+				p.number = number
+				must(updatePhone(db, p))
+			}
+		} else {
+			fmt.Println("No change required")
+		}
 	}
 }
 
@@ -98,6 +114,31 @@ func getPhone(db *sql.DB, id int) (string, error) {
 		return "", err
 	}
 	return number, nil
+}
+
+func findPhone(db *sql.DB, number string) (*phone, error) {
+	var p phone
+	row := db.QueryRow("SELECT * FROM phone_numbers WHERE value=$1", number)
+	err := row.Scan(&p.id, &p.number)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &p, nil
+}
+
+func updatePhone(db *sql.DB, p phone) error {
+	statement := `UPDATE phone_numbers SET value=$2 WHERE id=$1`
+	_, err := db.Exec(statement, p.id, p.number)
+	return err
+}
+
+func deletePhone(db *sql.DB, id int) error {
+	statement := `DELETE FROM phone_numbers WHERE id=$1`
+	_, err := db.Exec(statement, id)
+	return err
 }
 
 type phone struct {
