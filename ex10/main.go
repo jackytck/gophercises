893ef2c8f864d108e440df623b638e3077838760
Jacky Tang <jackytck@gmail.com>
2018-12-21 22:55:02 +0800

    :sparkles: add type: GameState + helper to clone it

--- ex10/main.go
+++ ex10/main.go
@@ -81,7 +81,6 @@ func main() {
 	// if dealer score <= 16, then hit
 	// if dealer has a soft 17, then hit
 	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		fmt.Println("here?")
 		card, cards = draw(cards)
 		dealer = append(dealer, card)
 	}
@@ -106,4 +105,45 @@ func main() {
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
+}
+
+type State int8
+
+const (
+	StatePlayerTurn = iota
+	StateDealerTurn
+	StateHandOver
+)
+
+// GameState represents the game state of the Blackjack game.
+type GameState struct {
+	Deck   []deck.Card
+	State  State
+	Dealer Hand
+	Player Hand
+}
+
+// CurrentPlayer return the address of the current player.
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case StateDealerTurn:
+		return &gs.Dealer
+	case StatePlayerTurn:
+		return &gs.Player
+	default:
+		panic("it isn't currently any player's turn")
+	}
+}
+
+func clone(gs GameState) GameState {
+	ret := GameState{
+		Deck:   make([]deck.Card, len(gs.Deck)),
+		State:  gs.State,
+		Dealer: make(Hand, len(gs.Dealer)),
+		Player: make(Hand, len(gs.Player)),
+	}
+	copy(ret.Deck, gs.Deck)
+	copy(ret.Dealer, gs.Dealer)
+	copy(ret.Player, gs.Player)
+	return ret
 }
