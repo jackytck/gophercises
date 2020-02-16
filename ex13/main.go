2203903d8c314d89ad22fbdd1bcd518e1b7132d0
Jacky Tang <jackytck@gmail.com>
2020-02-16 16:39:18 +0800

    :hammer: add mutex lock

--- ex13/main.go
+++ ex13/main.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/jackytck/gophercises/ex13/hn"
@@ -52,9 +53,12 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 var (
 	cache           []item
 	cacheExpiration time.Time
+	cacheMutex      sync.Mutex
 )
 
 func getCachedStories(numStories int) ([]item, error) {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
 	if time.Now().Sub(cacheExpiration) < 0 {
 		return cache, nil
 	}
@@ -63,7 +67,7 @@ func getCachedStories(numStories int) ([]item, error) {
 		return nil, err
 	}
 	cache = stories
-	cacheExpiration = time.Now().Add(1 * time.Second)
+	cacheExpiration = time.Now().Add(5 * time.Minute)
 	return cache, nil
 }
 
