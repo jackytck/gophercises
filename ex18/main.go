943c9ba2cb5c9b170c80f420d8a0702259b5d471
Jacky Tang <jackytck@gmail.com>
2020-03-23 01:12:12 +0800

    :construction_worker: web server for image upload

--- ex18/main.go
+++ ex18/main.go
@@ -1,33 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
+	"path/filepath"
 
 	"github.com/jackytck/gophercises/ex18/primitive"
 )
 
 func main() {
-	inFile, err := os.Open("008.jpg")
-	if err != nil {
-		panic(err)
-	}
-	defer inFile.Close()
-	out, err := primitive.Transform(inFile, 50)
-	if err != nil {
-		panic(err)
-	}
-	err = os.Remove("out.jpg")
-	if err != nil {
-		panic(err)
-	}
-	outFile, err := os.Create("out.jpg")
-	if err != nil {
-		panic(err)
-	}
-	defer outFile.Close()
-	_, err = io.Copy(outFile, out)
-	if err != nil {
-		panic(err)
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `<html><body>
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="image">
+				<button type="submit">Upload Image</button>
+			</form>
+			</body></html>
+		`
+		fmt.Fprint(w, html)
+	})
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		file, header, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		// @TODO
+		ext := filepath.Ext(header.Filename)
+		_ = ext
+		out, err := primitive.Transform(file, 50)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "image/jpg")
+		io.Copy(w, out)
+	})
+	port := "3000"
+	log.Printf("Listening at http://127.0.0.1:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
