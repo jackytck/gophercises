bf07d950fae389c69891e6013d1c2f0e40e26e0c
Jacky Tang <jackytck@gmail.com>
2020-04-04 17:06:16 +0800

    :hammer: svg bar chart

--- ex19/main.go
+++ ex19/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"math/rand"
 	"os"
-	"time"
 
 	svg "github.com/ajstarks/svgo"
 )
@@ -11,25 +10,44 @@ import (
 func rn(n int) int { return rand.Intn(n) }
 
 func main() {
-	f, err := os.OpenFile("demo.svg", os.O_RDWR|os.O_CREATE, 0755)
-	if err != nil {
-		panic(err)
+	canvas := svg.New(os.Stdout)
+	data := []struct {
+		Month string
+		Usage int
+	}{
+		{"Jan", 171},
+		{"Feb", 180},
+		{"Mar", 100},
+		{"Apr", 87},
+		{"May", 65},
+		{"Jun", 40},
+		{"Jul", 32},
+		{"Aug", 55},
+		{"Sep", 0},
+		{"Oct", 0},
+		{"Nov", 0},
+		{"Dec", 0},
 	}
-	defer f.Close()
-
-	canvas := svg.New(f)
-	width := 500
-	height := 500
-	nstars := 250
-	style := "font-size:48pt;fill:white;text-anchor:middle"
-
-	rand.Seed(time.Now().Unix())
+	width := len(data)*60 + 10
+	height := 300
+	threshold := 160
+	max := 0
+	for _, item := range data {
+		if item.Usage > max {
+			max = item.Usage
+		}
+	}
 	canvas.Start(width, height)
-	canvas.Rect(0, 0, width, height)
-	for i := 0; i < nstars; i++ {
-		canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
+	canvas.Rect(0, 0, width, height, "fill:white")
+	for i, val := range data {
+		percent := val.Usage * (height - 50) / max
+		canvas.Rect(i*60+10, (height-50)-percent, 50, percent, "fill:rgb(77,200,232)")
+		canvas.Text(i*60+35, height-24, val.Month, "font-size:14pt;fill:rgb(150, 150, 150);text-anchor:middle")
 	}
-	canvas.Circle(width/2, height, width/2, "fill:rgb(77, 117, 232)")
-	canvas.Text(width/2, height*4/5, "hello, world", style)
+	threshPercent := threshold * (height - 50) / max
+	canvas.Line(0, height-threshPercent, width, height-threshPercent, "stroke: rgb(255,100,100); opacity: 0.8; stroke-width: 2px")
+	canvas.Rect(0, 0, width, height-threshPercent, "fill:rgb(255, 100, 100); opacity: 0.1")
+	canvas.Line(0, height-50, width, height-50, "stroke: rgb(150, 150, 150); stroke-width:2")
+
 	canvas.End()
 }
